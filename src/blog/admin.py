f03b7a6543cd4c282c5574ed2b111588922190c7
aliaksandr <litvinaualex@gmail.com>
2023-08-02 13:08:01 +0300

    refactor Post model, add admin filter, search, ordering

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,21 @@
+# blog/admin.py
 from django.contrib import admin
+from .models import User, Post
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'user', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'user']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['user']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+
+admin.site.register(User, UserAdmin)  # @admin.register() performs the same function as admin.site.register()
--- src/blog/models.py
+++ src/blog/models.py
@@ -1,5 +1,6 @@
 # blog/models.py
 from django.db import models
+from django.utils import timezone
 
 
 class User(models.Model):
@@ -11,9 +12,26 @@ class User(models.Model):
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     title = models.CharField(max_length=200)
     body = models.TextField()
+    slug = models.SlugField(max_length=250)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
     def __str__(self):
         return self.title
